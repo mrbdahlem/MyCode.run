03b2eab99de0e2aee182020d7d3ca6afb4f17246
Mr. Dahlem <dahlem.brian@cusd80.com>
2017-04-16 00:23:18 -0700

    connected to AWS Lambda function

--- runcode.js
+++ runcode.js
@@ -4,15 +4,64 @@ function runCode() {
         apiKey: 'NfPLJMgMRZ59PS0Le1979LzuenRHCjN1KhvPup41'
     });
 
+    var className = $("#mainClass").val();
+    if (className === "" || className === undefined) className = "Main";
+    
     var body = {
-        //This is where you define the body of the request
+        version: 1,
+        compile: {
+            version: 1,
+            mainClass: className,
+            sourceFiles: [
+                            {
+                                name: className + '.java',
+                                contents: editor.getValue().split(/\r?\n/)
+                            }
+                        ]
+        },
+        "test-type": "run"
     };
-
-    apigClient.HelloFunctionPost(params, body, additionalParams)
+    
+    var params = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    };
+    
+    var additionalParams = {};
+    
+    var start = new Date().getTime();
+    
+    apigClient.helloFunctionPost(params, body, additionalParams)
         .then(function(result){
-           alert(result);
-        }).catch( function(result){
-           alert(result);
-            //This is where you would put an error callback
+    
+            var timeDiff = (new Date().getTime()) - start;
+            $('#exTime').html('Execution time: ' + (timeDiff) + 'ms');
+            
+            returntext = result.data.result;
+            
+            var success = 'Failed';
+            if (result.data.succeeded) {
+                success = 'Succeeded';
+            }
+            
+            $('#modalTitle').text('Execution ' + success);
+            $('#modalBody').html('<pre id="output">'+returntext+'</pre>');
+                
+            var display = ace.edit("output");
+            display.setOptions({
+                                maxLines: 25,
+                                minLines: 1,
+                                readOnly: true,
+                                highlightActiveLine: false,
+                                highlightGutterLine: false
+                            });
+        }).catch(function(result){
+            var timeDiff = (new Date().getTime()) - start;
+            $('#exTime').html('Execution time: ' + (timeDiff) + 'ms');
+            
+            $('#modalTitle').text('Execution Failed');
+            $('#modalBody').html('<p>Failure communicating with server</p>');
         });
+ 
+        $('#outputModal').modal('show');
 }
