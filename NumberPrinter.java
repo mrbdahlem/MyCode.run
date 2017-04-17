6b19fcd22d62e587695511b0e120e0996e064592
Mr. Dahlem <dahlem.brian@cusd80.com>
2017-04-16 21:40:09 -0700

    added second main test case

--- NumberPrinter.java
+++ NumberPrinter.java
@@ -1,5 +1,9 @@
 public class NumberPrinter {
     public NumberPrinter(int i) {
-	      System.out.println(i);
-	}
+        System.out.println(i);
+    }
+    
+    public static void main (String[] args) {
+        new NumberPrinter(23);
+    }
 }
