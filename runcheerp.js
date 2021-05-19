e99f94ad94c6e9624e053a675053cd8e4070085e
dahlem.brian <dahlem.brian@28T-F407-207718.cusd80.lz>
2021-05-19 08:51:18 -0700

    preparing for cheerpj local runner

---
+++ runcheerp.js
@@ -0,0 +1,44 @@
+/*
+ * Run the code contained in the file manager, by compiling everything and
+ * executing the main class, then display the result.
+ */
+/* global FileManager */
+
+function runCodeCheerp() {
+    // Get the main class's actual name..
+    // drop the .java extension
+    var mainName = FileManager.getMainFile().name.replace(/\.[^/.]+$/, "");
+    // Get the file's package name
+    var pkgReg = /package\s+([\w\.]+)\s*;/;
+    var packageName = pkgReg.exec(FileManager.getMainFile().contents);
+    // Add the package name to the class's name
+    if (packageName !== null && packageName.length >= 2) {
+        mainName = packageName[1] + "." + mainName;
+    }
+    
+    // Prepare the request to run the code
+    // Set up the request body for a compile-run request
+    var body = {
+        version: 1,
+        compile: {
+            version: 1,
+            mainClass: mainName,
+            sourceFiles: []
+        },
+        data: {
+            version: 1,
+            dataFiles: []
+        },
+        "test-type": "run"
+    };
+    
+    var root = FileManager.getRootFolder();
+    // Add the files in the global file manager to the request body.
+    addAllSourceFiles(root, body.compile.sourceFiles);
+    
+    // Add data files to the request
+    addAllDataFiles(root, body.data.dataFiles);
+    
+    let newWindow = window.open('./cheerpConsole.html');
+    newWindow.compileData = body;
+}
